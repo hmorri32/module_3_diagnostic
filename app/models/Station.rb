2e3b9ced5a36119e53f0b63b93486f7932ccaa8c
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 11:03:31 -0700

    Implement station model

---
+++ app/models/Station.rb
@@ -0,0 +1,11 @@
+class Station
+  attr_reader :fuel_type_code, :city, :station_name, :state, :distance
+
+  def initialize(data)
+    @fuel_type_code = data[:fuel_type_code]
+    @city           = data[:city]
+    @station_name   = data[:station_name]
+    @state          = data[:state]
+    @distance       = data[:distance]
+  end
+end
