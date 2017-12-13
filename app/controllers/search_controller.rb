c41249af8f776bc35f560bd3f8f7fbf735b6af13
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 11:03:25 -0700

    adds @stations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
+    @service  = NrelService.new(params[:q])
+    @stations = @service.closest_stations.map do |station|
+      Station.new(station)
+    end
   end
 end
