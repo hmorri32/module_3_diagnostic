478719bed9a45cbb652c57ec8a915e6892219382
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 11:03:54 -0700

    make use of VCR in user seach spec

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.feature "User searches" do
   describe "by zip code 80203" do
-    before { visit '/'; within('.navbar') { fill_in 'q', with: 80203; click_on "Locate"} }
-
     it "and clicks locate and its routes to /search with visible params" do
-      expect(current_path).to eq(search_path)
-      expect(page).to have_current_path("/search?utf8=%E2%9C%93&q=80203&commit=Locate")
+       VCR.use_cassette('user_searches/closest_stations', :match_requests_on => [:method]) do
+          visit '/'
+          within('.navbar') { fill_in 'q', with: 80203; click_on "Locate" }
+          expect(current_path).to eq(search_path)
+          expect(page).to have_current_path("/search?utf8=%E2%9C%93&q=80203&commit=Locate")
+        end
     end
 
     it "returns 10 closest stations within 6 miles and sorted by distance" do
-      
+      VCR.use_cassette('user_searches/returns_stations', :match_requests_on => [:method]) do
+        visit '/'
+        within('.navbar') { fill_in 'q', with: 80203; click_on "Locate" }
+        expect(page).to have_content('Station Name')
+        expect(page).to have_content('Fuel Type')
+        expect(page).to have_content('Distance')
+      end
     end
   end
 end
