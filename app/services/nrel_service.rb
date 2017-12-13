891568eadb4ca8306b99cf05b04e5c557cc07254
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 11:03:36 -0700

    deletes pseudocode

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -7,15 +7,8 @@ class NrelService
   def configure_faraday
     Faraday.new("https://developer.nrel.gov") do |f|
       f.adapter Faraday.default_adapter
-      # f.params[:api_key]    = ENV['NREL_API_KEY']
-      # f.params[:location]   = zip_code
-      # f.params[:limit]      = 10
-      # f.params[:distance]   = 6
-      # f.params[:fuel_types] = 'ELEC, LPG'
     end
   end
-
-  # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=0Bw0RAsakXPN1kquKkdAIge2pzhgDbqLiaLtSzMF&location=80203&limit=10&distance=6&fuel_types=ELEC,LPG
 
   def closest_stations
     get_json("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{zip_code}&limit=10&distance=6&fuel_types=ELEC,LPG")[:fuel_stations]
